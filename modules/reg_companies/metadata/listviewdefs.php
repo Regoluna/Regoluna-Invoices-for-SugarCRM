668f6776dc275dcf0e88d6f10d4e6b44b09c848d
Rodrigo Saiz <rodrigo@regoluna.com>
2014-09-05 11:44:15 +0200

    Better company listview in config section

--- modules/reg_companies/metadata/listviewdefs.php
+++ modules/reg_companies/metadata/listviewdefs.php
@@ -1,13 +1,43 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$module_name = 'reg_companies';
-$listViewDefs[$module_name] = array(
-	'NAME' => array(
-		'width' => '32', 
-		'label' => 'LBL_NAME', 
-		'default' => true,
-    'link' => true
-  ),         
+$listViewDefs ['reg_companies'] = 
+array (
+  'NAME' => array (
+    'width' => '10%',
+    'label' => 'LBL_NAME',
+    'default' => true,
+    'link' => true,
+  ),
+  'NAME2' => array (
+    'type' => 'varchar',
+    'label' => 'LBL_NAME2',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'NAME3' => array (
+    'type' => 'varchar',
+    'label' => 'LBL_NAME3',
+    'width' => '20%',
+    'default' => true,
+  ),
+  'NIF' => array (
+    'type' => 'varchar',
+    'label' => 'LBL_NIF',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'TYPE' => array (
+    'type' => 'enum',
+    'default' => true,
+    'studio' => 'visible',
+    'label' => 'LBL_TYPE',
+    'width' => '10%',
+  ),
+  'IS_DEFAULT' => array (
+    'default' => true,
+    'studio' => 'visible',
+    'label' => 'LBL_IS_DEFAULT',
+    'width' => '10%',
+  ),
 );
 ?>
