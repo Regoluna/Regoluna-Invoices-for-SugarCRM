40a3ca53ed39c8404ff95b9ecba3889e377db697
Rodrigo Saiz <rodrigo@regoluna.com>
2014-09-08 17:20:25 +0200

    Simplified reg_invoices class

--- modules/reg_invoices/reg_invoices.php
+++ modules/reg_invoices/reg_invoices.php
@@ -10,12 +10,64 @@
  * by the Free Software Foundation, version 3 of the License.
  *
  ********************************************************************************/
-require_once('modules/reg_invoices/reg_invoices_sugar.php');
-class reg_invoices extends reg_invoices_sugar {
+require_once('include/SugarObjects/templates/sale/Sale.php');
 
+class reg_invoices extends Sale {
+
+  var $new_schema = true;
+	var $module_dir = 'reg_invoices';
+	var $object_name = 'reg_invoices';
+	var $table_name = 'reg_invoices';
+	var $importable = false;
+  
+	var $disable_row_level_security = true ; // to ensure that modules created and deployed under CE will continue to function under team security if the instance is upgraded to PRO
+
+  var $id;
+  var $name;
+  var $date_entered;
+  var $date_modified;
+  var $modified_user_id;
+  var $modified_by_name;
+  var $created_by;
+  var $created_by_name;
+  var $description;
+  var $deleted;
+  var $created_by_link;
+  var $modified_user_link;
+  var $assigned_user_id;
+  var $assigned_user_name;
+  var $assigned_user_link;
+  var $reg_invoices_type;
+  var $lead_source;
+  var $amount;
+  var $amount_usdollar;
+  var $date_closed;
+  var $next_step;
+  var $sales_stage;
+  var $probability;
+  var $number;
+  var $discount;
+  var $iva;
+  var $retention;
+  var $state;
+  var $total_items;
+  var $total_discount;
+  var $total_base;
+  var $total_tax;
+  var $total_retention;
+  var $output_tax;
+  var $currency_id;
+  
   function reg_invoices() {
-    parent::reg_invoices_sugar();
+    parent::Sale();
   }
+  
+  function bean_implements($interface){
+		switch($interface){
+			case 'ACL': return true;
+		}
+		return false;
+	}
 
   function unformat_all_fields(){
     // Corregimos el comportamiento erroneo de SugarCRM en algunos casos.
